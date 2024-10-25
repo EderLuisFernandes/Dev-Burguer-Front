935a0598a7ab392c68c8da5c3d0115d71a27554e
Ederl010203 <eder.luis.1996@hotmail.com>
2024-10-25 16:09:59 -0300

    forntatualizado

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles"
+
+export function Footer(){
+
+    return <>
+        <Container>
+           
+                <p>Desenvolvido por Éder Luis - 2024 - Todos os direitos reservados</p>
+            
+        </Container>
+
+
+    </>
+}
